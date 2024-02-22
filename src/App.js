24c3a8844cc4b6a53163d8b8106c20b90dfa6988
Kert Laansalu <kertlaansalu@gmail.com>
2024-02-22 15:22:02 +0000

    removing garbage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import { Joystick } from "react-joystick-component";
 import Switch from '@mui/material/Switch';
 import axios from 'axios';
